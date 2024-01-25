a7ded039663d8c36dc4752938fdf8541247ce6dc
Geol1 <oldrichngnitedem4@gmail.com>
2024-01-25 13:33:32 +0100

    update

--- src/test/java/com/example/TestCaseGenerator2.java
+++ src/test/java/com/example/TestCaseGenerator2.java
@@ -159,6 +159,7 @@ public class TestCaseGenerator2 {
             double root = -b / (2 * a);
             return new double[] { root };
         } else {
+            
             double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
             double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
             return new double[] { root1, root2 };
